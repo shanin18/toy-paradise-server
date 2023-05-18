38ef5a4c678bf3d62a880dcf2d62ef37dbb3b586
shanin18 <112709243+shanin18@users.noreply.github.com>
2023-05-18 15:20:28 +0600

    implement get operation to find categories data

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gacal02.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,19 +17,29 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
-    const toyCategoriesCollection = client.db("toyParadise").collection("toyCategories");
+    const toyCategoriesCollection = client
+      .db("toyParadise")
+      .collection("toyCategories");
 
+    app.get("/action_figure/:category", async (req, res) => {
+      const category = req.params.category;
+      const query = {categoryName:category}
+      const result = await toyCategoriesCollection.find(query).toArray()
+      res.send(result)
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -38,11 +47,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("toy paradise server running...");
+});
 
-app.get("/", (req, res) =>{
-    res.send("toy paradise server running...")
-})
-
-app.listen(port, () =>{
-    console.log(`this server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`this server is running on port ${port}`);
+});
