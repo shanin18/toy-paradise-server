a20e2e853a5d4c5d12c46d9c3383218a097fa6a1
shanin18 <112709243+shanin18@users.noreply.github.com>
2023-05-19 21:15:44 +0600

    added get post delete operations

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gacal02.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,14 +27,56 @@ async function run() {
     const toyCategoriesCollection = client
       .db("toyParadise")
       .collection("toyCategories");
+    const allToyCollection = client.db("toyParadise").collection("allToys");
 
     app.get("/action_figure/:category", async (req, res) => {
       const category = req.params.category;
-      const query = {categoryName:category}
-      const result = await toyCategoriesCollection.find(query).toArray()
-      res.send(result)
+      const query = { subCategory: category };
+      const options = {
+        projection: { img: 1, title: 1, price: 1, ratings: 1 },
+      };
+      const result = await toyCategoriesCollection
+        .find(query, options)
+        .toArray();
+      res.send(result);
     });
 
+    app.get("/category/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCategoriesCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/allToys", async (req, res) => {
+      let query = {}
+      const options = {
+        projection: {
+          sellerName: 1,
+          title: 1,
+          subCategory: 1,
+          price: 1,
+          availableQuantity: 1,
+        },
+      };
+      const result = await allToyCollection.find(query, options).limit(20).toArray();
+      console.log(result)
+      res.send(result);
+    });
+
+    app.post("/allToys", async (req, res) => {
+      const allToys = req.body;
+      result = await allToyCollection.insertOne(allToys);
+      res.send(result);
+    });
+
+    app.delete("/allToys/:id", async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await allToyCollection.deleteOne(query);
+      res.send(result);
+    })
+   
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
