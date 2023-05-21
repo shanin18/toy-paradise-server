2bc5a6cbe995f69cb6f28ae1e17d2fd3b5c28234
shanin18 <112709243+shanin18@users.noreply.github.com>
2023-05-21 17:28:31 +0600

    fixed add a toy issue

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig))
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
@@ -29,17 +35,7 @@ async function run() {
       .collection("toyCategories");
     const allToyCollection = client.db("toyParadise").collection("allToys");
 
-    // Creating index on title field
-    const indexKey = { title: 1 };
-    const result = await allToyCollection.createIndex(indexKey);
-
-    app.get("/searchByToy/:text", async(req, res)=>{
-      const text = req.params.text;
-      const result = await allToyCollection.find({title: { $regex: text, $options: "i" }}).toArray()
-      res.send(result)
-
-    })
-
+    // getting data using sub-category
     app.get("/action_figure/:category", async (req, res) => {
       const category = req.params.category;
       const query = { subCategory: category };
@@ -52,6 +48,7 @@ async function run() {
       res.send(result);
     });
 
+    // getting single data using sub-category id
     app.get("/category/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -59,6 +56,7 @@ async function run() {
       res.send(result);
     });
 
+    // getting data for all toys
     app.get("/allToys", async (req, res) => {
       let query = {};
       const options = {
@@ -77,34 +75,44 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/allToys/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await allToyCollection.findOne(query)
+    // Creating index on title field
+    const indexKey = { title: 1 };
+    await allToyCollection.createIndex(indexKey);
+
+    // getting data using indexing on allToys
+    app.get("/searchByToy/:text", async (req, res) => {
+      const text = req.params.text;
+      const result = await allToyCollection
+        .find({ title: { $regex: text, $options: "i" } })
+        .toArray();
       res.send(result);
     });
 
+    // getting single data from all toys
+    app.get("/allToys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allToyCollection.findOne(query);
+      res.send(result);
+    });
 
-
+    // getting my toys data from all toys using email
     app.get("/myToys", async (req, res) => {
-      console.log(req.query.sort);
       let query = {};
       let options = {};
       if (req.query?.email) {
         query = { sellerEmail: req.query.email };
-        if(req.query.sort === "increasing"){
-          options = {price: 1 }
+        if (req.query.sort === "increasing") {
+          options = { price: 1 };
+        } else if (req.query.sort === "decreasing") {
+          options = { price: -1 };
         }
-        else if(req.query.sort === "decreasing"){
-          options = {price: -1 }
-        }
-        
       }
       const result = await allToyCollection.find(query).sort(options).toArray();
       res.send(result);
     });
 
-
+    // getting single data for set to default value on update form
     app.get("/update/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -112,12 +120,14 @@ async function run() {
       res.send(result);
     });
 
+    // adding data to all toys
     app.post("/allToys", async (req, res) => {
-      const allToys = req.body;
-      result = await allToyCollection.insertOne(allToys);
+      const data = req.body;
+      const result = await allToyCollection.insertOne(data);
       res.send(result);
     });
 
+    // updating single data from all toys
     app.put("/allToys/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -145,6 +155,7 @@ async function run() {
       res.send(result);
     });
 
+    // delete single data from all toys
     app.delete("/allToys/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
